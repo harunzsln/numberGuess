23438afee20d6babbd8d895ac0b418e9f27fe828
harunzsln <harunzsln0@gmail.com>
2024-03-21 13:57:40 +0300

    Add project files.

---
+++ numberGuess/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numberGuess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rounds = 10;
+            int randomNumber = new Random().Next(100) + 1;
+            int enteredNumber = 0;
+
+
+            while (rounds > 0)
+            {
+                Console.WriteLine("Guess the number ");
+                enteredNumber = Convert.ToInt32(Console.ReadLine());
+
+                if (enteredNumber > randomNumber)
+                {
+                    Console.WriteLine("Please enter a smaller number");
+                }
+                else if (enteredNumber < randomNumber)
+                {
+                    Console.WriteLine("Please enter a bigger number");
+                }
+                else
+                {
+                    Console.WriteLine("Congratulations! You win");
+                    Console.ReadLine();
+                    return;
+
+                }
+           
+            }
+
+            Console.WriteLine("You lose");
+            Console.ReadLine();
+
+
+        }
+    }
+}
